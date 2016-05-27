5abc1040ef310ab867d9d5dc35a4760c9a2e2a32
Jiyuan Liu <liujiyuan126@yeah.net>
2016-05-27 14:04:43 +0800

    Added icons. Modified launchscreen. Modified status bar color. Added calculator screen.

---
+++ ProgrammerCalc/ControlPad.swift
@@ -0,0 +1,18 @@
+//
+//  ControlPad.swift
+//  ProgrammerCalc
+//
+//  Created by LiuJiyuan on 5/25/16.
+//  Copyright © 2016 Joel Liu. All rights reserved.
+//
+
+import UIKit
+
+class ControlPad: UIView {
+    // MARK: Properties
+    
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
--- ProgrammerCalc/ViewController.swift
+++ ProgrammerCalc/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: Properties
+    @IBOutlet weak var numberScreen: UILabel!
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        // Change the status bar color
+        return UIStatusBarStyle.LightContent
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Change status bar color
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
