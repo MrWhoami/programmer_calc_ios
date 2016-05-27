d69456e46de6c80e53fb49615746d1564c9df713
Jiyuan Liu <liujiyuan126@yeah.net>
2016-05-27 16:02:58 +0800

    UI design finished. Added lots of buttons!

--- ProgrammerCalc/ViewController.swift
+++ ProgrammerCalc/ViewController.swift
@@ -20,7 +20,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        // Change status bar color
     }
 
     override func didReceiveMemoryWarning() {
